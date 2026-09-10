145a3bf3a9edf96c0581e4bc1c71cfd70c734fd0
Farjana3209 <sumrafarjana2@gmail.com>
2026-09-10 16:16:26 +0530

    update

--- js/firebase.js
+++ js/firebase.js
@@ -13,13 +13,15 @@ import {
 // YOUR FIREBASE CONFIG
 // ==========================================
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAwoHdf7BDZ_rAW9-baFfAkUIzQweJNCgI",
+  authDomain: "shopix-f1fee.firebaseapp.com",
+  projectId: "shopix-f1fee",
+  storageBucket: "shopix-f1fee.firebasestorage.app",
+  messagingSenderId: "927255606743",
+  appId: "1:927255606743:web:af5dd8de31c134c608c792",
+  measurementId: "G-6MQHBPQQX1"
 };
 
 
