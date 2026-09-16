79dd85bb895f0c76db8362850df77b0c6b2de165
Farjana3209 <sumrafarjana2@gmail.com>
2026-09-16 19:02:44 +0530

    updated code

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-storage.js";
 
 import {
@@ -13,14 +13,16 @@ import {
 // FIREBASE CONFIGURATION
 // ==========================================
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAwoHdf7BDZ_rAW9-baFfAkUIzQweJNCgI",
-    authDomain: "shopix-f1fee.firebaseapp.com",
-    projectId: "shopix-f1fee",
-    storageBucket: "shopix-f1fee.firebasestorage.app",
-    messagingSenderId: "927255606743",
-    appId: "1:927255606743:web:af5dd8de31c134c608c792",
-    measurementId: "G-6MQHBPQQX1"
+  apiKey: "AIzaSyAwoHdf7BDZ_rAW9-baFfAkUIzQweJNCgI",
+  authDomain: "shopix-f1fee.firebaseapp.com",
+  databaseURL: "https://shopix-f1fee-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "shopix-f1fee",
+  storageBucket: "shopix-f1fee.firebasestorage.app",
+  messagingSenderId: "927255606743",
+  appId: "1:927255606743:web:af5dd8de31c134c608c792",
+  measurementId: "G-6MQHBPQQX1"
 };
 
 
@@ -36,7 +38,7 @@ const app = initializeApp(firebaseConfig);
 // ==========================================
 
 const auth = getAuth(app);
-const db = getFirestore(app);
+const db = getDatabase(app);
 const storage = getStorage(app);
 
 
